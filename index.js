24ed8f72930cbccdb0ab22dc1e5badc2ee2663ed
priya <pmistry1707@gmail.com>
2022-01-06 16:05:13 +0000

    workshop complete

--- index.js
+++ index.js
@@ -35,7 +35,9 @@ export function removeItem(array, index) {
 //     updateName({ name: "Abe" }, "Barbara")
 // should give back:
 //     { name: "Barbara"}
-export function updateName(object, newName) {}
+export function updateName(object, newName) {
+	return { ...object, name: newName };
+}
 
 // Immutably update the object so that the value under the "needsACupOfTea" property becomes the opposite of what it was.
 // Any other properties in the object should be maintained.
@@ -43,14 +45,25 @@ export function updateName(object, newName) {}
 //     toggleTeaStatus({ name: "Abe", needsACupOfTea: false })
 // should give back:
 //     { name: "Abe", needsACupOfTea: true }
-export function toggleTeaStatus(object) {}
+export function toggleTeaStatus(object) {
+	return { ...object, needsACupOfTea: !object.needsACupOfTea };
+}
 
 // Combo Time!!
 
 // Immutably update the array as well as the object at the given position/index within the array, so that the value under the "completed" property becomes the opposite of what it was.
 // Any other properties in the object should be maintained.
 // For example calling:
-//    toggleListItemCompleted([{ task: "Cooking", completed: true }, { task: "Walking", completed: false }], 1)
+//    toggleListItemCompleted(
+// [{ task: "Cooking", completed: true },
+// { task: "Walking", completed: false }], 1)
 // should give back:
 //    [{ task: "Cooking", completed: true }, { task: "Walking", completed: true }]
-export function toggleListItemCompleted(array, index) {}
+export function toggleListItemCompleted(array, index) {
+	// array of object
+	// given an array and index find the object in the array
+	// create a new object using the spread operator and invert the completed value (toggle the value)
+	const oldObject = array[index];
+	const newObject = { ...oldObject, completed: !oldObject.completed };
+	return replaceItem(array, newObject, index);
+}
